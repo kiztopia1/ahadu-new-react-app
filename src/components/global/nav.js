cef25af639f1bdafe159d2fcb56ecae03d331f7e
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-15 12:30:04 -0700

    nav style updated

--- src/components/global/nav.js
+++ src/components/global/nav.js
@@ -4,33 +4,37 @@ import './nav.css'
 import {Link} from 'react-router-dom';
 const Nav = () => {
     return (
+        <div>
         <nav className="nav row">
-            <div className="logo col-xs-9 col-sm-9 col-md-9">
+            <div className="logo ">
                 <div className='burger'>
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
-                Ahadu
+                AHADU
             </div>
 
-            <div className="other col-sx-3">
-                <div><span className='fa fa-user'></span> sign in</div>
-                <div className="chart"><span className="fa fa-shoppingcart"></span></div>
-                
-            </div>
+            
 
             <div className="search ">
                 <input type="text"></input>
                 <button>search</button>
             </div>
-            <div className='col-xs-12 col-sm-12'>
-            <Link to='/book' className='white'>book</Link>
-            <Link to='/' className="white">store</Link>
-            <Link to='/reader' className="white">reader</Link>
-            <Link to='/signIn' className='white'>sign in</Link>
+
+            <div className="other">
+                <div><span className='fa fa-user'></span> sign in</div>
+                <div className="chart"><span className="fa fa-shoppingcart"></span></div>
             </div>
+            
         </nav>
+        <div className='col-xs-12 col-sm-12'>
+        <Link to='/book' className='white'>book</Link>
+        <Link to='/' className="white">store</Link>
+        <Link to='/reader' className="white">reader</Link>
+        <Link to='/signIn' className='white'>sign in</Link>
+        </div>
+        </div>
     )
 }
 
